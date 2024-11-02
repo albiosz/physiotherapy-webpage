3655b84d0ed8bf1334f3eb890888de0bba5e6311
albiosz <szulcalbert@gmail.com>
2024-11-02 15:25:02 +0100

    optimize services-small HTML + CSS

---
+++ fill-data.js
@@ -0,0 +1,27 @@
+const data = {
+  services: [
+    {
+      title: "Terapia manualna",
+      img: "/img/services/terapia-manulana.jpg",
+      text: "Terapia manualna w bólach kręgosłupa, następstwach urazów narządu ruchu, operacjach, wadach postawy",
+      href: "/terapia-manualna/"
+    },
+    {
+      title: "Terapia neurofizjologiczna",
+      img: "/img/services/terapia-neurofizjologiczna.jpg",
+      text: "Terapia PNF, NDT-BOBATH w chorobach neurologicznych",
+      href: "/terapia-neurofizjologiczna"
+    },
+    {
+      title: "Trening funkcjonalny",
+      img: "/img/services/trening-funkcjonalny.jpg",
+      text: "",
+      href: "/trening-funkcjonalny"
+    },
+  ]
+} 
+
+
+let template = document.getElementById('offer').innerHTML;
+let rendered = Mustache.render(template, data);
+document.getElementById('offer').innerHTML = rendered;
